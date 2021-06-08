dea3c33c1773c84fe9f93d79ae92ce778aa3a654
Tinashe "Viz" Matembo <31240908+Vizz03@users.noreply.github.com>
2021-06-08 17:18:05 +0200

    Added Authentication

---
+++ src/core/config.js
@@ -0,0 +1,9 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC0q_qyrIqcdsosCGYWDhrE5YATWpiDqXo",
+  authDomain: "savings-app-f9c75.firebaseapp.com",
+  projectId: "savings-app-f9c75",
+  storageBucket: "savings-app-f9c75.appspot.com",
+  messagingSenderId: "971495048143",
+  appId: "1:971495048143:web:fb78269485b27b4aadc8ba"
+}
