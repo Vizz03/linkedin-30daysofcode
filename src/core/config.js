7f429a30059a057e78af1fd2ce6a8e7d38693917
Tinashe "Viz" Matembo <31240908+Vizz03@users.noreply.github.com>
2021-06-09 17:35:56 +0200

    Added Bottom Tab Navigation

--- src/core/config.js
+++ src/core/config.js
@@ -1,9 +1,9 @@
 // Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC0q_qyrIqxxxxxxxxxxxxxxxxx",
-  authDomain: "savings-app-fxxxxxxxxxxxx.com",
-  projectId: "savings-app-xxxxxxxxxx",
-  storageBucket: "savings-app-xxxxxxxxxx.appspot.com",
-  messagingSenderId: "xxxxxxxxxxx",
-  appId: "1:9714950xxxxxxxxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyC0q_qyrIqcdsosCGYWDhrE5YATWpiDqXo",
+  authDomain: "savings-app-f9c75.firebaseapp.com",
+  projectId: "savings-app-f9c75",
+  storageBucket: "savings-app-f9c75.appspot.com",
+  messagingSenderId: "971495048143",
+  appId: "1:971495048143:web:fb78269485b27b4aadc8ba"
 }
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,23 +1,66 @@
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { logoutUser } from '../api/auth-api'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
-export default function Dashboard() {
+const Dashboard = () => {
+  function HomeScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>This is where the Savings data will come</Text>
+      </View>
+    )
+  }
+
+  function SettingsScreen() {
+    return (
+      <Background>
+        <Logo />
+        <Header>Account Settings</Header>
+        <Paragraph>This is where account settings will come</Paragraph>
+        <Button mode="outlined" onPress={logoutUser}>
+          Logout
+        </Button>
+      </Background>
+    )
+  }
+
+  const Tab = createBottomTabNavigator()
+
   return (
-    <Background>
-      <Logo />
-      <Header>Let’s start</Header>
-      <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
-      </Paragraph>
-      <Button mode="outlined" onPress={logoutUser}>
-        Logout
-      </Button>
-    </Background>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline'
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'person' : 'person-outline'
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   )
 }
+
+export default Dashboard
+
+const styles = StyleSheet.create({})
